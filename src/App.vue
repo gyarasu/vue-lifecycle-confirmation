f78f6bf0b9f1c37f7bdf27cd426e7a0ea40d02ae
hiroki.yoshitsugu <hiroki.yoshitugu@persol.co.jp>
2019-05-14 18:38:12 +0900

    confirm the behavior of v-if and mounted

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <my-component :param=1 v-if="comp1"></my-component>
+    <my-component :param=2 v-if="comp2"></my-component>
+    <button v-on:click="switch1">normal</button>
+    <button v-on:click="switch2">new thread</button>
+    <button v-on:click="reset">reset</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponent.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    'my-component': MyComponent,
+  },
+  data() {
+    return {
+      comp1: true,
+      comp2: false,
+    };
+  },
+  methods: {
+    switch1() {
+      // my-component for 'param=2' won't be mounted
+      this.comp1 = false;
+      this.comp2 = true;
+    },
+    switch2() {
+      this.comp1 = false;
+
+      // my-component for 'param=2' will be mounted
+      setTimeout(() => {
+        this.comp2 = true;
+      }, 1);
+    },
+    reset() {
+      location.reload();
+    },
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    param: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      message: '',
+    };
+  },
+  beforeCreate() {
+    console.log('[beforeCreate]');
+  },
+  created() {
+    console.log('[created]');
+  },
+  beforeMount() {
+    console.log('[beforeMount]');
+  },
+  mounted() {
+    console.log('[mounted]');
+    if (this.param === 1) {
+      this.message = 'Hello';
+    } else {
+      this.message = 'こんにちは';
+    }
+  },
+  beforeUpdate() {
+    console.log('[beforeUpdate]');
+  },
+  updated() {
+    console.log('[updated]');
+  },
+  beforeDestroy() {
+    console.log('[beforeDestroy]');
+  },
+  destroyed() {
+    console.log('[destroyed]');
+  }
+}
+</script>
